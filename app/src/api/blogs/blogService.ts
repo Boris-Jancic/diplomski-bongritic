9e3d2ba9b425c66f936f8d6bd6c1f83a63fe185f
Boris Jančić <jancicboris56@gmail.com>
2022-05-22 21:30:10 +0100

    Added new api calls

--- app/src/api/blogs/blogService.ts
+++ app/src/api/blogs/blogService.ts
@@ -1,10 +1,17 @@
 
-import axios from "axios";
 import { Blog } from "../../interface/post";
+import AxiosClient from "../client/axiosClient";
 
 const BASE_URL = process.env.REACT_APP_API_KEY
 
-export const postReviewerPost = async (post: Blog.CreatePost) => await axios.post<Blog.CreatePost>(`${BASE_URL}/posts`, post)
-export const getReviewerPosts = async () => await axios.get<Array<Blog.PostResponse>>(`${BASE_URL}/posts`)
-export const getReviewerPost = async (id: string) => await axios.get<Blog.Post>(`${BASE_URL}/posts/one?id=${id}`)
-export const getLatestReviewerPost = async () => await axios.get<Blog.Post>(`${BASE_URL}/posts/latest`)
+export const postReviewerPost = async (post: Blog.CreatePost) => await AxiosClient.post<Blog.CreatePost>(`${BASE_URL}/posts`, post)
+
+export const getReviewerPosts = async () => await AxiosClient.get<Array<Blog.PostResponse>>(`${BASE_URL}/posts`)
+
+export const getReviewerPost = async (id: string) => await AxiosClient.get<Blog.Post>(`${BASE_URL}/posts/one?id=${id}`)
+
+export const getLatestReviewerPost = async () => await AxiosClient.get<Blog.Post>(`${BASE_URL}/posts/latest`)
+
+export const getCriticReviews = async (email: string) => await AxiosClient.get<any>(`${BASE_URL}/reviewers/comments?email=${email}`)
+
+export const getCriticAverageGrade = async (gameId: string) => await AxiosClient.get<any>(`${BASE_URL}/posts/average/reviewers/grade?id=${gameId}`)
