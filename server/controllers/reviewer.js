74be44b462d8c3b662c2bf0ec0265a6c61466560
Boris Jančić <jancicboris56@gmail.com>
2022-05-22 21:37:44 +0100

    Fixed naming client => reviewer

--- server/controllers/reviewer.js
+++ server/controllers/reviewer.js
@@ -4,7 +4,19 @@ import Reviewer from '../models/reviewer.js'
 
 export const getReviewers = async (req, res) => { 
     try {
-        const Reviewers = await Client.find();
+        const Reviewers = await Reviewer.find();
+        console.log("Getting Reviewers")
+        res.status(200).json(Reviewers);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const getAvatar = async (req, res) => { 
+    const { email } = req.query
+    console.log(email)
+    try {
+        const Reviewers = await Reviewer.findBy();
         console.log("Getting Reviewers")
         res.status(200).json(Reviewers);
     } catch (error) {
