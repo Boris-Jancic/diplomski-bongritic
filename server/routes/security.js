72a41ed63079d1f7de1db135ef964d2056e019b6
Boris Jančić <jancicboris56@gmail.com>
2022-06-27 16:22:06 +0100

    Cleaned and optimized authentification and validation checks

---
+++ server/routes/security.js
@@ -0,0 +1,29 @@
+import { validationResult } from "express-validator";
+
+export function checkClient(req, res, next) {
+    const { authorization } = req.headers 
+
+    if (authorization !== "CLIENT") return res.status(403).json({message:"You do not have this permision"});
+
+    return next();
+}
+
+export function checkReviewer(req, res, next) {
+    const { authorization } = req.headers 
+    
+    if (authorization !== "REVIEWER") return res.status(403).json({ message: error.message });
+
+    return next();
+}
+
+export function badRequestHandler(req, res, next) {
+    const error = validationResult(req).formatWith(({ msg }) => msg);
+
+    const hasError = !error.isEmpty();
+    if (hasError) {
+    console.log(error.array())
+        res.status(422).json({ messages: error.array() });
+    } else {
+        next();
+    }
+}
